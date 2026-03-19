af1704f436d316309967ff8d9012c8b6de40baae
ROSHINI AARTHI <roshiniaarthirk@gmail.com>
2026-03-19 09:48:39 +0530

    Added result page and folder structure

---
+++ js/result.js
@@ -0,0 +1,27 @@
+// Get score
+let score = localStorage.getItem("score") || 0;
+
+// Show score
+document.getElementById("score").textContent = score;
+
+// Message
+let msg = document.getElementById("message");
+
+if(score >= 8){
+    msg.textContent = "Excellent!";
+}
+else if(score >= 5){
+    msg.textContent = "Good Job!";
+}
+else{
+    msg.textContent = "Try Again!";
+}
+
+// Buttons
+function retryQuiz(){
+    window.location.href = "question.html";
+}
+
+function viewAnswers(){
+    window.location.href = "summary.html";
+}
